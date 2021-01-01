d645e0aa4769fb2f11e8ca176a4c11c000a74921
Toba <toba_ojo@hotmail.com>
2021-01-01 20:09:00 +0000

    Add files via upload

---
+++ website8/editPost.php
@@ -0,0 +1,65 @@
+<?php
+require('config/db.php');
+require('config/config.php');
+
+if(isset($_POST['submit'])){
+    $update_id = mysqli_real_escape_string($conn, $_POST['update_id']);
+    $title = mysqli_real_escape_string($conn, $_POST['title']);
+    $author = mysqli_real_escape_string($conn, $_POST['author']);
+    $body = mysqli_real_escape_string($conn, $_POST['body']);
+
+    $query = "UPDATE posts SET 
+    title = '$title',
+    author='$author',
+    body = '$body'
+    WHERE id ={$update_id}
+    ";  
+
+    if(mysqli_query($conn, $query)){
+        header('location: '.ROOT_URL.'');
+    } else {
+        echo 'error'.mysql_error($conn);
+    }
+}
+$id = mysqli_real_escape_string($conn, $_GET['id']);
+
+$query = "SELECT * FROM posts WHERE id=".$id;
+
+$res = mysqli_query($conn, $query);
+
+$post = mysqli_fetch_assoc($res);
+//var_dump($posts);
+
+mysqli_free_result($res);
+
+mysqli_close($conn);
+
+
+?>
+
+<?php include('inc/header.php')?>
+<div class="container">
+<h1>Edit Ticket</h1>
+<form method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>" class="p-4">
+  <fieldset>
+    <legend>Please edit your ticket: </legend>
+    </div>
+    <div class="form-group">
+      <label for="author">Name</label>
+      <input name="author" type="text" class="form-control" id="exampleInputName1" aria-describedby="nameHelp" value="<? echo $post['author']?>">
+    </div>
+    <div class="form-group">
+      <label for="title">Title</label>
+      <input name="title" type="text" class="form-control" id="exampleInputName1" aria-describedby="nameHelp" value="<? echo $post['title']?>">
+    </div>
+    <div class="form-group">
+      <label for="body"> Message:</label>
+      <textarea name="body" class="form-control" id="exampleTextarea" rows="3"><?echo $post['body']?>
+      </textarea>
+    </div>
+    <input type="hidden" name="update_id" value="<? echo $post['id']?>">
+    <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+  </fieldset>
+</form>
+  </div>
+<?php include('inc/footer.php')?>
---
+++ website8/post.php
@@ -0,0 +1,50 @@
+<?php
+require('config/db.php');
+require('config/config.php');
+
+if(isset($_POST['delete'])){
+    $delete_id = mysqli_real_escape_string($conn, $_POST['delete_id']);
+
+    $query = "DELETE from posts WHERE id={$delete_id}";  
+
+    if(mysqli_query($conn, $query)){
+        header('location: '.ROOT_URL.'');
+    } else {
+        echo 'error'.mysqli_error($conn);
+    }
+}
+
+
+$id = mysqli_real_escape_string($conn, $_GET['id']);
+
+$query = "SELECT * FROM posts WHERE id=".$id;
+
+$res = mysqli_query($conn, $query);
+
+$post = mysqli_fetch_assoc($res);
+//var_dump($posts);
+
+mysqli_free_result($res);
+
+mysqli_close($conn);
+?>
+
+<?php include('inc/header.php')?>
+<div class="container">
+<a class="btn btn-secondary" href="<?php echo ROOT_URL;?>">Back</a>
+    <div class="card border-primary mt-3" style="max-width: 200rem;">
+    <div class="card-header p-2">Ticket ID: <?echo $post['id']?></div>
+    <h4 class="card-title p-2"><?php echo $post['title'];?></h4>
+    <hr>
+    <p class="card-text p-2"><?php echo $post['body']; ?></p>
+    <hr>
+    <small class="p-2">Created on: <?php echo $post['created_at']; ?> by: <?php echo $post['author']; ?></small>
+    </div>
+    <hr>
+    <a href="<?php echo ROOT_URL;?>editPost.php?id=<?php echo $post['id'];?>" class="btn btn-primary" >Edit</a>
+    <hr>
+    <form class="pull-right" method="POST" action="<?echo $_SERVER['PHP_SELF']?>">
+    <input type="hidden" name="delete_id" value="<?echo $post['id']?>">
+    <input type="submit" name="delete" value="Delete" class="btn btn-danger">
+    </form>
+<?php include('inc/footer.php')?>
